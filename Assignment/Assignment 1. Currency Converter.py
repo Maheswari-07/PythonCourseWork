761a7c694608928d842d66510ed288e33ff27f8d
maheswari <21kq1a0521@pace.ac.in>
2025-07-23 14:54:27 +0530

    currency converter

--- Assignment/Assignment 1. Currency Converter.py
+++ Assignment/Assignment 1. Currency Converter.py
@@ -38,3 +38,34 @@ print(f"Supported Currencies: {', '.join([cur.strip() for cur in supported_curre
 print("\nAgent Details: Name - {}, Contact - {}, Location - {}".format(
     agent_details["agent_name"], agent_details["agent_contact"], agent_details["agent_location"]
 ))
+
+
+#Input Details:
+    
+#Enter Conversion ID: 501
+#Enter Your Name: Kusuma
+#Enter From Currency (e.g., USD): USD
+#Enter To Currency (e.g., INR): INR
+#Enter Amount to Convert: 200
+#Enter Conversion Rate: 85.2
+#Enter Fee Percentage: 2.5
+#Enter Supported Currencies (comma-separated): USD,INR,EUR,GBP
+#Enter Agent Name: forxKing
+#Enter Agent Contact Number: 9014462548
+#Enter Agent Location: Ap
+
+#OUTPUT:
+
+#--- Conversion Summary ---
+#Conversion ID, User, Amount:,501,Kusuma,200.0
+#Fee Applied: 2.50%
+
+#From: USD
+#To: INR
+#Amount: 200.0
+#Conversion Rate: 85.2
+#Converted Amount (Before Fee): 17040.0
+#Fee Amount: ₹5.0
+#Supported Currencies: INR, EUR, USD, GBP
+
+#Agent Details: Name - forxKing, Contact - 9014462548, Location - Ap
